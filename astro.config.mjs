b3549e5f32455e7b2e24e621424de3afae1dbcec
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-01-27 15:33:45 -0300

    Actualizacion, corrección

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
 	site: 'https://este-blog.vercel.app/',
 	integrations: [mdx(), sitemap()],
 });
